9214cad925dce6b2e69ae2670d2a7b3df90b8fa2
Claudius26 <osomzed@gmail.com>
2024-12-08 23:25:48 -0800

    Add files via upload

---
+++ taskeight.py
@@ -0,0 +1,11 @@
+sumtaskseven = 0
+for number in range(1,11):
+	if (number % 4 == 0):
+		value = 1
+		sumtasksix = 0
+		for _ in range(5):
+			value *= number
+			sumtasksix += value
+		sumtaskseven += sumtasksix
+
+print(sumtaskseven)
